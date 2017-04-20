f1b3cc5afcf772b5957e5d1cc5ad809ab38041ce
Mateusz Kumanowski <mateusz@imag.pl>
2017-04-20 17:19:27 +0200

    back end project

---
+++ license-manager/DataBaseContext.cs
@@ -0,0 +1,26 @@
+﻿
+using licensemanager.Model.DataBaseModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MySQL.Data.Entity.Extensions;
+using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// The entity framework context with a DataBaseContext DbSet
+/// </summary>
+public class DataBaseContext : DbContext
+        {
+        
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            var configuration = builder.Build();
+
+            string connectionString = configuration.GetConnectionString("DbConnection");
+
+                optionsBuilder.UseMySQL(connectionString);
+            }
+            public DbSet<Users> Users { get; set; }
+        }
+
---
+++ license-manager/Model/DataBaseModel/Users.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace licensemanager.Model.DataBaseModel
+{
+    public class Users
+    {
+        public Users()
+        {
+        }
+
+        public int Id { get; set; }
+
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+
+    }
+}
